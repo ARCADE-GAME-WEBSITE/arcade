0947d7ce8e48c0e242a440f06ceecc5f78f83cbd
Bùi Duy Phương <65779951+bduyphuong12@users.noreply.github.com>
2021-10-19 21:46:52 +0700

    Add files via upload
    dropdown

---
+++ App.js
@@ -0,0 +1,59 @@
+
+import React from "react";
+import {
+  Collapse,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  Navbar,
+  NavLink,
+  Nav,
+  Container,
+} from "reactstrap";
+
+function App(props) {
+  const [collapseOpen] = React.useState(false);
+  const [color] = React.useState("navbar-transparent");
+  return (
+    <>
+      <Navbar className={( color)} expand="lg">
+        <Container >
+          <Collapse navbar isOpen={collapseOpen}>
+            <Nav className="ml-auto" navbar>
+              <UncontrolledDropdown nav>
+                <DropdownToggle
+                  caret
+                  color="default"
+                  nav
+                  onClick={(e) => e.preventDefault()}
+                >
+                  <div className="photo">
+                    <img
+                      alt="..."
+                      src={require("assets/img/friend-img3.jpg").default}
+                    />
+                  </div>
+                </DropdownToggle>
+                <DropdownMenu className="dropdown-navbar" right tag="ul">
+                  <NavLink tag="li">
+                    <DropdownItem className="nav-item">Profile</DropdownItem>
+                  </NavLink>
+                  <NavLink tag="li">
+                    <DropdownItem className="nav-item">Settings</DropdownItem>
+                  </NavLink>
+                  <DropdownItem divider tag="li" />
+                  <NavLink tag="li">
+                    <DropdownItem className="nav-item">Log out</DropdownItem>
+                  </NavLink>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default App;
