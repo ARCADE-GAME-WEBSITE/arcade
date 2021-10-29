3b0196b4c7f45f69b5582fdc7de46902ae7fab73
bduyphuong12 <65779951+bduyphuong12@users.noreply.github.com>
2021-10-30 02:00:10 +0700

    take data from database to website

--- front-end/user_website/src/Pages/UserAccount/UserProfile/change-password.jsx
+++ front-end/user_website/src/Pages/UserAccount/UserProfile/change-password.jsx
@@ -1,32 +1,38 @@
 import React, { Component } from "react";
 import './change-password.css'
-
+import './user-profile'
 class ChangePassword extends Component {
     state = {}
     handleSubmit = e => {
         e.preventDefault();
     }; 
+    onCloseForm = () =>{
+        this.props.onCloseForm();
+    }
 
     render() { 
         return (
-            <form onSubmit={this.handleSubmit} id="reset-password-form">
+            <form onSubmit={this.handleSubmit} id="popup-form fixed-top">
                 <div className="auth-inner">
                     <h3>Reset Password</h3>
-
+                    <label 
+                    className="close-btn" 
+                    onClick={this.onCloseForm} 
+                    >&times;</label>
                     <div className="form-group">
                         <label className="form-label">New Password:</label>
                         <input type="password" className="form-control" placeholder="New password"
                             onChange={e => this.email = e.target.value}/>
                     </div>
 
-                    <div className="form-group">
+                    <div className="form-group">    
                         <label className="form-label">Confirm New Password:</label>
                         <input type="password" className="form-control" placeholder="Confirm new password"
                             onChange={e => this.password = e.target.value}/>
                     </div>
 
                     <div className="form-group">
-                        <button className="btn btn-primary btn-block">Reset Password</button>
+                        <button className="btn btn-primary form-btn">Reset Password</button>
                     </div>
                 </div>
             </form>
--- front-end/user_website/src/Pages/UserAccount/user-account.jsx
+++ front-end/user_website/src/Pages/UserAccount/user-account.jsx
@@ -2,44 +2,58 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import SideBar from "./SideBar/sidebar";
 import routes from "./user-account-routes";
+import PlayedGames from "./PlayedGames/played-games";
+import Records from "./PersonalRecord/personal-record";
+import UserProfile from "./UserProfile/user-profile";
+import ListFriend from "./FriendList/friend-list";
+
 import { BackgroundColorContext } from "./BackgroundColorContext";
 
 
-function UserAccount({props}) {
+
+function UserAccount({props,user})  {
   
   const mainPanelRef = React.useRef(null);
-  const getRoutes = (routes) => {
-    return routes.map((prop, key) => {
-      if (prop.layout === "/user-account") {
-        return (
-          <Route
-            path={prop.layout + prop.path}
-            component={prop.component}
-            key={key}
-          />
-        );
-      } else {
-        return null;
-      }
-    });
-  };
+  // const getRoutes = (routes) => {
+  //   return routes.map((prop, key) => {
+  //     if (prop.layout === "/user-account") {
+  //       return (
+  //         <Route
+  //           path={prop.layout + prop.path}
+  //           component={prop.component}
+  //           key={key}
+  //         />
+  //       );
+  //     } else {
+  //       return null;
+  //     }
+  //   });
+  // };
   return (
+
+     
     <BackgroundColorContext.Consumer>
       {({ color }) => (
         <React.Fragment>
           <div className="wrapper">
             <SideBar
               routes={routes}
+              user={user}
             />
             <div className="main-panel" ref={mainPanelRef} data={color} >
-              <Switch>
-                {getRoutes(routes)}
+              <Switch > 
+                {/* {getRoutes(routes) */}
+                <Route path='/user-account/user-profile' component={() => <UserProfile  user={user}  /> }/>
+                <Route path='/user-account/records' component={() => <Records  /> }/>
+                <Route path='/user-account/friends-list' component={() => <ListFriend  /> }/>
+                <Route path='/user-account/played-games' component={() => <PlayedGames /> }/>
               </Switch>
             </div>
           </div>
         </React.Fragment>
       )}
     </BackgroundColorContext.Consumer>
+
   );
 }
 
