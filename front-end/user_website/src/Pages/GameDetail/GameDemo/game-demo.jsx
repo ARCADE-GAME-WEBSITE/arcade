5c18856cdbd6794abd2a5d64604fb1ebb08cb78c
Lê Trung Nghĩa <90697464+KyoNo2k1@users.noreply.github.com>
2021-11-05 16:17:31 +0700

    img

--- front-end/user_website/src/Pages/GameDetail/GameDemo/game-demo.jsx
+++ front-end/user_website/src/Pages/GameDetail/GameDemo/game-demo.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 import './game-demo.css'
 
-function GameDemo({game}) {
+function GameDemo({game,gameDemoUrl}) {
     // axios.get('/game/')
     //     .then((res) => {})
     //     .catch((err) => {
@@ -15,7 +15,7 @@ function GameDemo({game}) {
             <div className="game-demo game-desc">
                 <h3>Video demo</h3>
                 <div className="game-demo__link">
-                    <iframe src='' width="300" height="160" title="The YouTube video player"frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe src={gameDemoUrl} width="300" height="160" title="The YouTube video player"frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
