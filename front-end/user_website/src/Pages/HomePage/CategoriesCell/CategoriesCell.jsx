6a8b1115190efe99af521b92c245d436c0914015
HiImNear <19521953@gm.uit.edu.vn>
2021-11-06 21:38:09 +0700

    update famous game + gametype

--- front-end/user_website/src/Pages/HomePage/CategoriesCell/CategoriesCell.jsx
+++ front-end/user_website/src/Pages/HomePage/CategoriesCell/CategoriesCell.jsx
@@ -7,12 +7,12 @@ function CategoriesCell({categories}){
     const categoriesAvaUrl = axios.defaults.baseURL + 'uploads/images/games/Categories/' + categories.CategoryName;
 
     return (
-        
+        <div className="game-type--css">
                     <a className="game-type-link" href ={'/home/' + categories.CategoryName} >
                         <img className="game-type__img" src={categoriesAvaUrl} alt="Cannot get categories image" />
                         <div className="game-type__name">{categories.CategoryName}</div>
                     </a>
-        
+        </div>
     );
 }
 
