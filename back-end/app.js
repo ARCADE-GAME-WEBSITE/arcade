1cf1410c4e412e3e61841987e29b6a34fdcf9010
Nguyễn Đặng Tuấn Kiệt <87163945+AmoryBunzz@users.noreply.github.com>
2021-11-15 14:46:57 +0700

    Fix cors

--- back-end/app.js
+++ back-end/app.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://arcadegame-gonin.netlify.app/']
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://arcadegame-gonin.netlify.app']
 }))
 
 app.use(express.json());
